b5d84d52767fee07952ccdd9a3004cccba56a588
shreya-gb <61507305+shreya-gb@users.noreply.github.com>
2021-11-29 21:53:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-// import { useEffect } from 'react'
+import SawoLogin from 'sawo-react';
 
-export default function Home() {
-      <title>SAWO Authentication</title>
+const Greetings = () => {
+    function sawoLoginCallback(payload) {
+        console.log(payload);
+    }
+    
+const sawoConfig = {
+    onSuccess: sawoLoginCallback, //required,
+    identifierType: 'email', //required, must be one of: 'email', 'phone_number_sms',
+    apiKey: 'e431edf0-4132-4f5d-9f2f-b2ddd834405e', // required, get it from sawo dev.sawolabs.com,
+    containerHeight: '750px', // the login container height, default is 300px
+  }
+
   return (
-      <div>
-        <h1 align="center" width= {50} height= {50}> Hello! Welcome to your NextJS Dashboard! </h1>
-      </div>
-  )
-}
+    <div>
+        <SawoLogin config={sawoConfig}/>
+    </div>
+  );
+};
+
+export default Greetings;
